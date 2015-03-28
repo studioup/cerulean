1946038e1c70eaecc7e7244ff06197767e70728e
Stefano Marchesi <marchesi@studioup.it>
2015-03-28 10:38:53 +0100

    indenting footer.php

--- footer.php
+++ footer.php
@@ -1,29 +1,29 @@
 
 
 <div class="full-width footer-widget">
-	<div class="row">
-		<?php dynamic_sidebar("Footer"); ?>
-	</div>
+    <div class="row">
+        <?php dynamic_sidebar("Footer"); ?>
+    </div>
 </div>
 
 <footer id="main-footer" class="full-width" role="contentinfo">
-	<!--
+    <!--
     <div class="row">
-		<div class="large-12 columns">
-			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
-		</div>
-	</div>
-	-->
-	<div class="credits">
-    	<div class="row">
-    		<div class="large-8 small-12 medium-8 columns">
-    			<p>&copy; <?php echo date('Y'); ?> Business Name <small>- <?php _e('VAT ID','cerulean');?> xxxxxxxx.</small> </p>
-    		</div>
-    		<div id="credits" class="large-4 small-12 medium-4 columns">
-                <a href="http://www.studioup.it/" title="Studio Up - Web Design Milano"><strong>Studio Up - Web Agency Milano</strong></a>  	
-    		</div>
-    	</div>
-	</div>
+        <div class="large-12 columns">
+            <?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
+        </div>
+    </div>
+    -->
+    <div class="credits">
+        <div class="row">
+            <div class="large-8 small-12 medium-8 columns">
+                <p>&copy; <?php echo date('Y'); ?> Business Name <small>- <?php _e('VAT ID','cerulean');?> xxxxxxxx.</small> </p>
+            </div>
+            <div id="credits" class="large-4 small-12 medium-4 columns">
+                <a href="http://www.studioup.it/" title="Studio Up - Agenzie Web Milano"><strong>Studio Up - Agenzia Web Milano</strong></a>
+            </div>
+        </div>
+    </div>
 </footer>
 <div id="outdated">
      <h6><?php _e('Your browser is out-of-date!','cerulean'); ?></h6>
@@ -32,15 +32,16 @@
 </div>
 
 <?php wp_footer(); ?>
-
+<!--
 <script>
     //$('body');
-	(function($) {
-		//$(document).foundation();
+    (function($) {
+        //$(document).foundation();
         //console.log('test')
-        
-	})(jQuery);
+
+    })(jQuery);
 </script>
-	
+-->
+
 </body>
 </html>
