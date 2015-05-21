a83ac623eb92f8d23f08aa1feb24003d3555abf4
Stefano Marchesi <marchesi@studioup.it>
2015-05-21 14:53:41 +0200

    ulteriore fix forms checkbox e radio

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 function frmThemeOverride_frmPlaceError(key,errObj){
     //jQuery('#frm_field_'+key+'_container').append('<small class="error frm_error">'+errObj[key]+'</small>');
-     jQuery( '<small class="error frm_error">'+errObj[key]+'</small>' ).insertAfter( jQuery('#frm_field_'+key+'_container input:last-child, #frm_field_'+key+'_container textarea:last-child') );
+    if(jQuery('#frm_field_'+key+'_container .frm_checkbox,#frm_field_'+key+'_container .frm_radio').length > 0 ){
+        jQuery( '<small class="error frm_error">'+errObj[key]+'</small>' ).insertAfter( jQuery('#frm_field_'+key+'_container .frm_radio:last, #frm_field_'+key+'_container .frm_checkbox:last') );
+    }else{
+        jQuery( '<small class="error frm_error">'+errObj[key]+'</small>' ).insertAfter( jQuery('#frm_field_'+key+'_container input:last, #frm_field_'+key+'_container textarea:last') );
+    }     
 }
 
 (function($) {
