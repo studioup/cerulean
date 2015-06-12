07cbc32cfe6637ad3138eddf963a005a621c18c2
Stefano Marchesi <marchesi@studioup.it>
2015-06-12 15:03:06 +0200

    Revert a4c3814..ca8b8a0
    This rolls back to commit a4c381408fb76e4bfaf38c18141863183151e31a.

--- js/include.cookiesEnabler.js
+++ js/include.cookiesEnabler.js
@@ -3,19 +3,19 @@ COOKIES_ENABLER.init({
     bannerHTML: '<div class="ce-banner__inner">'+
                     '<h4 id="ce-banner__header">'+
                         '<div class="cookie-ita">Questo sito internet utilizza cookies</div>'+
-                        '<div class="cookie-eng" style="display:none">This website uses cookies</div>'+
+                        '<div class="cookie-eng" >This website uses cookies</div>'+
                     '</h4>'+
                     '<div class="cookie-ita">Questo sito internet utilizza cookies per migliorare la vostra esperienza d’uso. Utilizzando il nostro sito internet autorizza l’installazione dei cookies in accordo con la nostra Cookie Policy.</div>'+
-                    '<div class="cookie-eng" style="display:none">This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.</div>'+
+                    '<div class="cookie-eng" >This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.</div>'+
                     '<br>'+
                     '<div class="ce-banner__buttons">'+
                         '<a href="#" class="ce-trigger">'+
                             '<div class="cookie-ita">Accetto</div>'+
-                            '<div class="cookie-eng" style="display:none">Enable cookies</div>'+
+                            '<div class="cookie-eng">Enable cookies</div>'+
                         '</a>'+
                         '<a class="ce-more" target="_blank"  href="http://privacy.studioup.it/cookies-policy/?url='+ homeUrl + '" target="_self">'+
                             '<div class="cookie-ita">Maggiori informazioni</div>'+
-                            '<div class="cookie-eng" style="display:none">Privacy policy</div>'+
+                            '<div class="cookie-eng" >Privacy policy</div>'+
                         '</a>'+
                     '</div>'+
                 '</div>',
