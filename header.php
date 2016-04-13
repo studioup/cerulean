4815d8aceec74025b76aa30a2b7eb4496058cfa6
Stefano Marchesi <kesy85@gmail.com>
2016-04-13 18:44:48 +0200

    aggiunto analytics di default ma commentato

--- header.php
+++ header.php
@@ -59,6 +59,19 @@
 </head>
 
 <body id="main-body" <?php body_class('antialiased'); ?> data-toggler=".menu-expanded">
+<!--
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-00000000-1', 'auto');
+  ga('set', 'anonymizeIp', true)
+  ga('send', 'pageview');
+
+</script>
+-->
     <div class="animsition">
 	    <?php get_template_part( 'parts-nav/menu', 'centered-float-logo-float-search' ); ?>
         
