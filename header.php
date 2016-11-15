a593e73e743d2c2901e4c1719180fc991eaacc4b
Stefano Marchesi <kesy85@gmail.com>
2016-11-15 23:33:58 +0100

    googletagmanager

--- header.php
+++ header.php
@@ -6,6 +6,15 @@
 <!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> > <!--<![endif]-->
 <head>
+	<?php if(!empty(constant("GOOGLE_TAG_ID"))){ ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','<?php echo constant("GOOGLE_TAG_ID");?>');</script>
+	<!-- End Google Tag Manager -->
+	<?php } ?>
 	<meta charset="<?php bloginfo('charset'); ?>">
 
 	<title><?php wp_title(); ?></title>
@@ -59,19 +68,12 @@
 </head>
 
 <body id="main-body" <?php body_class('antialiased'); ?> data-toggler=".menu-expanded">
-<!--
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-00000000-1', 'auto');
-  ga('set', 'anonymizeIp', true)
-  ga('send', 'pageview');
-
-</script>
--->
+	<?php if(!empty(constant("GOOGLE_TAG_ID"))){ ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo constant("GOOGLE_TAG_ID");?>"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php } ?>
     <div class="animsition">
 	    <?php get_template_part( 'parts-nav/menu', 'centered-float-logo-float-search' ); ?>
         
